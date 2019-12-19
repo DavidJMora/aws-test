a9c232c1d07cb221747e10dddea6befe80afdef1
David Mora <David.mora@codeimmersives.com>
2019-12-19 12:12:48 -0500

    might of fixed the issue where the front end and back end could not communicate with each other

--- idle-game-frontend/src/lib/Axios.js
+++ idle-game-frontend/src/lib/Axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const Axios = axios.create({
-    baseURL: process.env.NODE_ENV === 'development' ? 'http://codeimmersives.info' : '',
+    baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3001/api' : '',
     timeout: 50000
 });
 
